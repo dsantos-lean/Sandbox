2699d19b89d4852df4110a08dbbddf4808d5b3ff
dsant <lean.delossantos@my.jcu.edu.au>
2023-05-17 12:29:51 +1000

    Add Re-learning directory, move default py files to default-sandbox directory

--- Default-sandbox/list_files.py
+++ Default-sandbox/list_files.py
@@ -1,7 +1,7 @@
 import os
 
 print(f"The files and folders in {os.getcwd()} are:")
-items = os.listdir('.')
+items = os.listdir('..')
 for item in items:
     prefix = "(d) " if os.path.isdir(item) else "(f) "
     print(f"{prefix}\t{item}")
