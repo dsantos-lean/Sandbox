a135f70dca19466b6bb1945fd9a6ff74be421d1b
dsant <lean.delossantos@my.jcu.edu.au>
2023-08-19 09:15:34 +1000

    Refactor parse_attributes

--- task1_dgn_parser_FINAL.py
+++ task1_dgn_parser_FINAL.py
@@ -38,6 +38,8 @@ def parse_label(lines):
     # <label> ::= <letter> { <letter> }
     if isinstance(lines, list):
         line = lines.pop(0)
+        if line == '\n':
+            line = parse_label(lines)
         line = line.strip()
     else:
         line = lines
@@ -51,30 +53,43 @@ def parse_label(lines):
 def parse_attribute(lines):
     # <attribute> ::= <label> : <number>
     line = lines.pop(0)
-    line = line.strip()
-    parts = line.rsplit(" ", 1)
-    attribute_label = parse_label(parts[0])
-    number = parse_number(parts[1])
-    # result = " ".join([attribute_label, str(number)])
-    result = attribute_label, str(number)
-    return result
+    while line != '\n':
+        line = line.strip()
+        parts = line.rsplit(" ", 1)
+        attribute_label = parse_label(parts[0])
+        number = parse_number(parts[1])
+        # result = " ".join([attribute_label, str(number)])
+        result = attribute_label, str(number)
+        return result
 
 
 def parse_attributes(lines):
     # <attributes> ::= <indentation> <attribute> {"\n" <indentation> <attribute>} "\n"
     attributes = {}
-    while lines[0] != '\n':
+    while len(lines) > 0:
+        if lines[0] == '\n':
+            return attributes
         attribute = [parse_attribute(lines)]
-        attributes.update(attribute)
+        if None not in attribute:
+            attributes.update(attribute)
     return attributes
+    # if len(attributes) == 0:
+    #     del attributes
 
-def parse_node(lines):
-    graph = {}
-    label = parse_label(lines)
-    attributes = parse_attributes(lines)
-    graph[label] = {}
-    graph[label] = attributes
+def parse_node(lines, graph):
+    print(f'Hello {graph}')
+    while len(lines) > 0:
+        label = parse_label(lines)
+        attributes = parse_attributes(lines)
+        graph[label] = {}
+        graph[label].update(attributes)
     return graph
+
+# def parse_edge(lines):
+#     line = parse_label(lines)
+#     return line
+
+# def item(lines, graph):
 
 
 
@@ -83,5 +98,8 @@ def parse_node(lines):
 # print(parse_label(lines))
 # print(parse_attribute(lines))
 # print(parse_attributes(lines))
-print(parse_node(lines))
+# print(parse_node(lines))
 # dgn_parser('source.txt')
+graph = {}
+print(parse_node(lines, graph))
+# print(item(lines, graph))
