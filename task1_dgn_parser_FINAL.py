34307d5495a0e7009a55edb916cfca57a81e395b
dsant <lean.delossantos@my.jcu.edu.au>
2023-08-20 08:32:23 +1000

    Finalize file, added dgn_parser function

--- task1_dgn_parser_FINAL.py
+++ task1_dgn_parser_FINAL.py
@@ -1,9 +1,14 @@
 import string
 
-# def dgn_parser(filename):
-with open('source.txt') as file:
-    lines = file.readlines()
-    # lines.copy()
+
+graph = {}
+
+
+def dgn_parser(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        graph = item(lines)
+    print(graph)
 
 
 def parse_number(line):
@@ -41,12 +46,12 @@ def parse_label(lines):
         if line == '\n':
             line = parse_label(lines)
         line = line.strip()
+        common_letters = set(line) & set(string.ascii_letters)
+        if len(common_letters) == 0:
+            raise ValueError("invalid label")
     else:
         line = lines
-    common_letters = set(line) & set(string.ascii_letters)
 
-    if len(common_letters) == 0:
-        raise ValueError("invalid label")
     return line
 
 
@@ -59,7 +64,7 @@ def parse_attribute(lines):
         attribute_label = parse_label(parts[0])
         number = parse_number(parts[1])
         # result = " ".join([attribute_label, str(number)])
-        result = attribute_label, str(number)
+        result = attribute_label, number  # str(number)
         return result
 
 
@@ -76,30 +81,50 @@ def parse_attributes(lines):
     # if len(attributes) == 0:
     #     del attributes
 
-def parse_node(lines, graph):
-    print(f'Hello {graph}')
+
+def parse_node(label, lines, graph):
+    # print(f'Hello {graph}')
     while len(lines) > 0:
-        label = parse_label(lines)
+        # label = parse_label(lines)
         attributes = parse_attributes(lines)
         graph[label] = {}
         graph[label].update(attributes)
+        item(lines)
     return graph
 
-# def parse_edge(lines):
-#     line = parse_label(lines)
-#     return line
 
-# def item(lines, graph):
+def parse_edge(label, lines, graph):
+    inner_graph = {}
+    graph_keys = [keys for keys in graph.keys()]
+    parts = label.split(": ", 1)
+    node_label = parts[0]
+    edge_label = parts[1]
+    if node_label in graph_keys:
+        attributes = parse_attributes(lines)
+        inner_graph[edge_label] = {}
+        inner_graph[edge_label].update(attributes)
+        graph[node_label].update(inner_graph)
+    return graph
 
 
+def item(lines):
+    label = parse_label(lines)
+    if ':' in label:
+        result = parse_edge(label, lines, graph)
+    else:
+        result = parse_node(label, lines, graph)
+    return result
 
+
+# Testing the different functions
 # print(f'This should be Jack Smith, result: {parse_label(lines)}')
 # print(parse_attribute(lines))
 # print(parse_label(lines))
 # print(parse_attribute(lines))
 # print(parse_attributes(lines))
 # print(parse_node(lines))
-# dgn_parser('source.txt')
-graph = {}
-print(parse_node(lines, graph))
-# print(item(lines, graph))
+# print(parse_node(lines, graph))
+# parse_edge(lines)
+
+
+dgn_parser('source.txt')
